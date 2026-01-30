0b7457c389e3c9060e3a678d5f951345dcf32f42
미나리 <cc360653@gmail.com>
2026-01-31 01:06:02 +0900

    ci: GitHub Pages 배포 자동화 및 정적 빌드 설정
    - GitHub Actions 워크플로우(deploy.yml) 추가
    - Next.js 정적 사이트 내보내기(static export) 옵션 활성화

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  //output: "export", //정적파일로 내보내기
+  output: "export", //정적파일로 내보내기
   images: {
     unoptimized: true, //github.io라면 최적화서버 없어서 T
   },
